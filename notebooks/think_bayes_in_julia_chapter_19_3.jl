b283a3988d37da5f5a29764cff55a9f80417d76e
Bill Allen <bill@k2bea.org>
2022-07-24 11:42:42 -0400

    start on clustering

--- notebooks/think_bayes_in_julia_chapter_19_3.jl
+++ notebooks/think_bayes_in_julia_chapter_19_3.jl
@@ -23,16 +23,31 @@ using Distributions, Plots
 lam = Gamma(1.4, 1)
 
 # ╔═╡ ad5ddc2b-3ff9-4691-afde-da5b5f72c9fd
-function model(alpha, beta, n)
+function prior(alpha, beta, n)
 	lam = Gamma(alpha, beta)
 	[rand(Poisson(x)) for x in rand(lam, n)]
 end
 
 # ╔═╡ 50451c2a-ec77-45b1-9a40-fb516fbd7bb1
-gs = model(1.4, 1, 1000)
+gs = prior(1.4, 1, 1000)
 
 # ╔═╡ fbd6e222-c0a3-49a4-aa32-a5d25e4e9c61
 plot(cdf_from_seq(gs), seriestype=:step, xaxis=[0,2,4,6,8,10])
+
+# ╔═╡ 0d7e6f5c-1cd8-474b-b83a-aba472c772a1
+function posterior(alpha, beta, n, data)
+	lam = Gamma(alpha, beta)
+	[pdf(Poisson(x), data) for x in rand(lam, n)]
+end
+
+# ╔═╡ 6543e0b5-5140-4b8f-b449-06466c4f7a4b
+post = posterior(1.4, 1, 1000, 4)
+
+# ╔═╡ 781fa062-1fea-4106-a806-03bbc793f98a
+post2 = round.(post * 50)
+
+# ╔═╡ 8da0c603-ee28-4457-a945-ec67a497bb6c
+plot(cdf_from_seq(post2), seriestype=:step)
 
 # ╔═╡ Cell order:
 # ╠═6d67c447-e64b-4506-bc55-30ba9f29bef5
@@ -42,3 +57,7 @@ plot(cdf_from_seq(gs), seriestype=:step, xaxis=[0,2,4,6,8,10])
 # ╠═ad5ddc2b-3ff9-4691-afde-da5b5f72c9fd
 # ╠═50451c2a-ec77-45b1-9a40-fb516fbd7bb1
 # ╠═fbd6e222-c0a3-49a4-aa32-a5d25e4e9c61
+# ╠═0d7e6f5c-1cd8-474b-b83a-aba472c772a1
+# ╠═6543e0b5-5140-4b8f-b449-06466c4f7a4b
+# ╠═781fa062-1fea-4106-a806-03bbc793f98a
+# ╠═8da0c603-ee28-4457-a945-ec67a497bb6c
